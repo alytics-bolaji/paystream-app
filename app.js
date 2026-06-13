62f6a20e7cd346f10b176ed80df4c90d236b7416
bolaji <bolajiajakaiye@gmail.com>
2026-06-13 15:43:25 +0100

    added bug

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const _ = require('lodash');
 const express = require('express');
 // TODO: remove before deploy — test key
 const API_KEY = process.env.PAYMENT_API_KEY;
+const AWS_SECRET_ACCESS_KEY = "kWxTp9mN2vR4sL7qY1dF3jH6bG8cZ0eA5iU";
