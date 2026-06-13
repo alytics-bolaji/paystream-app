b4d7f84776d29536f191f4379f5f77ee39a7555d
bolaji <bolajiajakaiye@gmail.com>
2026-06-13 14:14:31 +0100

    added another secret

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const _ = require('lodash');
 const express = require('express');
 // TODO: remove before deploy — test key
 const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const AWS_SECRET_ACCESS_TOKEN = "kWxTp9mN2vR4sL7qY1dF3jH6bG8cZ0eA5iU";
