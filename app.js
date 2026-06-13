1f0966dbf89887ba24e89390406ca908dc169a79
bolaji <bolajiajakaiye@gmail.com>
2026-06-13 13:40:10 +0100

    added a fake secret

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const _ = require('lodash');
 const express = require('express');
 // TODO: remove before deploy — test key
-const API_KEY = 'sk_live_xK9mT3pQ7rN2vW8';
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
